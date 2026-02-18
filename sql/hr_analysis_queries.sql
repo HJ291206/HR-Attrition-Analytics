3f1244944c04e5531a442736c474d82add7845d8
HJ291206 <harshiljauhari@gmail.com>
2026-02-19 01:53:03 +0530

    Add SQL queries for attrition analysis

--- sql/hr_analysis_queries.sql
+++ sql/hr_analysis_queries.sql
@@ -1,1 +1,22 @@
+-- Overall Attrition Rate
+SELECT 
+COUNT(CASE WHEN attrition='Yes' THEN 1 END)*100.0/COUNT(*) AS attrition_rate
+FROM employees;
+
+-- Attrition by Department
+SELECT department, COUNT(*) AS total_attrition
+FROM employees
+WHERE attrition='Yes'
+GROUP BY department;
+
+-- Salary vs Attrition
+SELECT attrition, AVG(monthly_income) AS avg_income
+FROM employees
+GROUP BY attrition;
+
+-- Job Satisfaction vs Attrition
+SELECT job_satisfaction, COUNT(*) AS total_attrition
+FROM employees
+WHERE attrition='Yes'
+GROUP BY job_satisfaction;
 
